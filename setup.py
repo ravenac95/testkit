5e796a497bd2a7d22e461aba68b321fb48159c5b
Reuven V. Gonzales <reuven@tobetter.us>
2012-05-25 13:04:47 -0700

    Added generic project scaffolding

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+VERSION = '0.0.1-dev'
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(name='testkit',
+    version=VERSION,
+    description="testkit - A collection of tools for testing",
+    long_description=LONG_DESCRIPTION,
+    author='Reuven V. Gonzales',
+    author_email='reuven@tobetter.us',
+    url="https://github.com/ravenac95/testkit",
+    license='MIT',
+    platforms='Ubuntu',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    entry_points={},
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+)
