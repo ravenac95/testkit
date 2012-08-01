c69e0417b43e946b510692e48f1b84064b01e31b
Reuven V. Gonzales <reuven@tobetter.us>
2012-08-01 10:43:32 -0700

    PEP8 fixes

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,8 +1,10 @@
 from testkit.context import *
 
+
 def test_context_user():
     from contextlib import contextmanager
     test_dict = dict(value='before')
+
     @contextmanager
     def test_context(test_dict):
         test_dict['value'] = 'during'
@@ -14,34 +16,41 @@ def test_context_user():
     assert test_dict['value'] == 'during'
     ctx.exit()
     assert test_dict['value'] == 'after'
-    
+
+
 class my_context(ContextDecorator):
     def before(self):
         self.hello = 'hello'
         self.done = False
-    
+
     def after(self):
         self.done = True
 
+
 def test_context_decorator_as_decorator():
     as_decorator = my_context()
+
     @as_decorator
     def hello(context):
         assert context.hello == 'hello'
     hello()
     assert as_decorator.done == True
 
+
 def test_context_decorator_as_decorator_exception():
     as_decorator = my_context()
     fake_message = 'A fake error!'
+
     @as_decorator
     def hello(context):
         raise Exception(fake_message)
+
     try:
         hello()
     except Exception, e:
         assert e.message == fake_message
     assert as_decorator.done == True
+
 
 def test_context_decorator_as_context():
     as_context = my_context()
@@ -49,6 +58,7 @@ def test_context_decorator_as_context():
         assert context.hello == 'hello'
         assert context.done == False
     assert context.done == True
+
 
 def test_context_decorator_as_context_exception():
     as_context = my_context()
@@ -60,14 +70,16 @@ def test_context_decorator_as_context_exception():
         assert e.message == fake_message
     assert context.done == True
 
+
 class my_other_context(ContextDecorator):
     def before(self):
         self.hello = 'hello'
         self.done = False
         return self.hello
-    
+
     def after(self):
         self.done = True
+
 
 def test_context_decorator_before_returns_custom_context():
     as_context = my_other_context()
